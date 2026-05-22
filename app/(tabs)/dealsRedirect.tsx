fe648ccc93266e46e31e296bdda68c7bf50c89b5
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-23 01:07:43 +0300

    chore: remove all files and assets from the dotSokoApp directory

--- app/(tabs)/dealsRedirect.tsx
+++
@@ -1,5 +0,0 @@
-import { Redirect } from "expo-router";
-
-export default function DealsRedirect() {
-  return <Redirect href="/deals" />;
-}
--- app/(tabs)/shopRedirect.tsx
+++
@@ -1,5 +0,0 @@
-import { Redirect } from "expo-router";
-
-export default function ShopRedirect() {
-  return <Redirect href="/shop" />;
-}
